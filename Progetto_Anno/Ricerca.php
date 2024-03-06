74f4c43e112dc709d7a37a8d2dcf23e1fce99bc0
SinghGurpreet02 <148762968+SinghGurpreet02@users.noreply.github.com>
2024-03-06 13:09:56 +0100

    Add files via upload
    Commi del 06-03-2024 Aggiungendo le classi operatori e cittadini

--- Progetto_Anno/Ricerca.php
+++ Progetto_Anno/Ricerca.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <head>  
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
 
     <?php //inclusioni di file esterni
         include("connessione.php");
@@ -15,55 +15,33 @@
     ?>
 
     <title>Interfaccia</title>
-    <style>
-        .ricerca{  
-  			margin-top: 10px;
-  		}
-        .container_Ricerca{
-            display: flex;
-        }
-        .divider {
-            flex: 1;
-            height: 1px;
-            background-color: #ccc;
-            margin: 10px;
-        }
-
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        select {
-            width: 50%;
-            margin-top: 10px;
-        }
-    </style>
 </head>
 <body action="Ricerca.php" method="$POST">
-
-    
-    
-    <?php
-        echo" <label for='Ricerca'>Mostra i dati degli:</label> 
-        <select id='Filtro' name='Filtro' style='height: 30px'> 
-            <option value='1'>Operatori</option> 
-            <option value='2'>Cittadini</option>
-        </select> ";
-        
-        
-        
-    ?>
-
-    <nav>
+    <nav class="nav">
         <a href="#">Home</a>
         <a href="#">Ricerca</a>
     </nav>
 
-    <div class="container">
-        <div class="divider divider-1"></div>
-        <div class="divider divider-2"></div>
-        <table>
+    <?php
+        echo" <label for='Ricerca'>Mostra i dati degli:</label> 
+        <select id='Filtro' name='Filtro' style='height: 30px'> 
+            <option value='1'>Operatori</option> 
+            <option value='2'>Cittadini</option>    
+        </select> ";
+    ?>
+<br><br>
+<label for="Ricerca">Ricerca Per:</label> 
+            <select id="Operatori" name="Operatori" style="height: 30px"> 
+                <option value="1">Nome</option> 
+                <option value="2">Cognome</option>
+            </select>
+        <br><br>
+        <input type="text" class="ricerca" id="Ricerca"style="height: 25px">
+        <br><br>
+        <input type="button" class="ricerca" id= "cerca" value="Cerca" style="height: 25px">
+
+    
+        <table class="tabel">
             <thead>
             <?php //stampa campi tabella inziali e dati rispettivi
                 echo "
@@ -80,26 +58,13 @@
             <tbody>
                 <tr>
                     <td><input type="checkbox"></td>
-                    <td class='item-cerchiato'>Mario</td>
+                    <th class='item-cerchiato'>Mario</td>
                     <td class='item-cerchiato'>Rossi</td>
                     <td class='item-cerchiato'>01/01/1990</td>
                     <td class='item-cerchiato'>ABC123456789</td>
                 </tr>
             </tbody>
         </table>
-    </div >
-    <div class="container_Ricerca">
-        <br><br>
-        <label for="Ricerca">Ricerca Per:</label> 
-            <select id="Operatori" name="Operatori" style="height: 30px"> 
-                <option value="1">Nome</option> 
-                <option value="2">Cognome</option>
-            </select>
-        <br><br>
-        <input type="text" class="ricerca" id="Ricerca"style="height: 25px">
-        <br><br>
-        <input type="button" class="ricerca" id= "cerca" value="Cerca" style="height: 25px">
-    </div>
-
+        
 </body>
 </html>
---
+++ Progetto_Anno/cittadini/delete.php
@@ -0,0 +1,53 @@
+<?php
+/* --- DELETE DI UN OGGETTO ESISTENTE ---
+
+Posso richiamare questo servizio soltanto da Postman:
+1. impostare il metodo HTTP DELETE
+2. immettere l'URL della richiesta aggiungendo ?isbn=x
+*/
+
+// TODO: Utili in fase di debug, da rimuovere nel deploy
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+require_once("../inc/database.inc.php");
+require_once("../models/cittadino.php");
+require_once("../inc/sanitize.inc.php");
+require_once("../inc/responsemessage.inc.php");
+require_once("../inc/tokenvalID_cittadiniator.inc.php"); // OPZIONALE per rispondere solo ad utenti autenticati con token
+
+$database = new Database();
+$db = $database->getConnection();
+
+// Il TokenValID_cittadiniator è necessario solo quando il servizio risponde ad utenti autenticati 
+if($database->GetAuthenticated() && !Tokenvalidator($conn,$user)){
+	http_response_code(401);
+	exit;
+}
+
+if(isset($_GET['ID_cittadini'])) // Recupero l'ID_cittadini dalla querystring
+{
+	$cittadini = new Cittadini($db);
+	$cittadini->ID_cittadini = $_GET['ID_cittadini'];
+
+	if($cittadini->delete()){
+		http_response_code(200); // OK (https://it.wikipedia.org/wiki/Codici_di_stato_HTTP)
+		$rspMsg = new Responsemessage("cittadini eliminato correttamente", $cittadini->ID_cittadini); 
+	} else {
+		http_response_code(503); //503 service unavailable
+		$rspMsg = new Responsemessage("Impossibile eliminare il cittadini", -1); 
+	}
+} else {
+	http_response_code(503); //503 service unavailable
+	$rspMsg = new Responsemessage("ID_cittadini non indicato", -1); 
+}
+
+echo json_encode($rspMsg); // in alternativa echo json_encode(array("message" => "Testo del messaggio"));
+?>
